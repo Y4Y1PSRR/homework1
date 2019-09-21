44440e7475989ba1e78a98445ed9ee2f46eef6a0
Y4Y1PSRR <55439681+Y4Y1PSRR@users.noreply.github.com>
2019-09-22 05:24:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+//RUMUS LUAS SEGI TIGA = ALAS * TINGGI / 2
+$X = 4; // ALAS SEGI TIGA
+$Y = 8; // TINGGI SEGI TIGA
+echo $X * $Y / 2; ?>
+
+<?php
+//RUMUS LUAS LINGKARAN = 22/7 * JARI-JARI * JARI-JARI
+$H = 22/7;
+$H = 22/7;
+$A = 7; // JARI-JARI LINGKARAN
+echo $H*$A*$A;
+?>
+
+<?php
+//RUMUS LUAS JAJAR GENJANG = ALAS * TINGGI
+$P = 12; // ALAS JG
+$R = 7; // TINGGI JG
+echo $P * $R;
+?>
